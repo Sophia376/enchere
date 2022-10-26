1be2a5f9076174ca064b7d9f10d41bd5a7610213
sespinolabenit01 <sespinolabenit01@pc-0290.csip.in>
2022-10-26 15:39:38 +0200

    ajouter clients

--- src/main/java/fr/insa/espinola/infos3/GestionbD.java
+++ src/main/java/fr/insa/espinola/infos3/GestionbD.java
@@ -6,6 +6,8 @@ package fr.insa.espinola.infos3;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -16,6 +18,7 @@ import java.util.logging.Logger;
  * @author sespinolabenit01
  */
 public class GestionbD {
+
     public static Connection connectGeneralPostGres(String host,
             int port, String database,
             String user, String pass)
@@ -33,8 +36,8 @@ public class GestionbD {
             throws ClassNotFoundException, SQLException {
         return connectGeneralPostGres("localhost", 5439, "postgres", "postgres", "pass");
     }
-    
-    public static void creeSchema(Connection con)
+
+    public static void creeUtilisateur(Connection con)
             throws SQLException {
         // je veux que le schema soit entierement cree ou pas du tout
         // je vais donc gerer explicitement une transaction
@@ -43,72 +46,116 @@ public class GestionbD {
             // creation des tables
             st.executeUpdate(
                     """
-                    create table Clients (
+                    create table clients (
                         id integer not null primary key
                         generated always as identity,
-                        nom varchar(30) not null unique,
+                        nom varchar(30) not null,
+                        prenom varchar(30) not null,
+                        email varchar(30) not null unique,
+                        codepostal varchar(30) not null,
                         pass varchar(30) not null
                     )
                     """);
-            
-            con.commit();
-            // je retourne dans le mode par defaut de gestion des transaction :
-            // chaque ordre au SGBD sera considere comme une transaction independante
+
+            con.commit(); // je retourne dans le mode par defaut de gestion des transaction : chaque ordre au SGBD sera considere comme une transaction independante
             con.setAutoCommit(true);
-        } catch (SQLException ex) {
-            // quelque chose s'est mal passe
-            // j'annule la transaction
-            con.rollback();
-            // puis je renvoie l'exeption pour qu'elle puisse eventuellement etre geree (message a  l'utilisateur...)
+        } catch (SQLException ex) { // quelque chose s'est mal passe j'annule la transaction
+            con.rollback(); // puis je renvoie l'exeption pour qu'elle puisse eventuellement etre geree (message a  l'utilisateur...)
             throw ex;
-        } finally {
-            // je reviens a  la gestion par defaut : une transaction pour chaque ordre SQL
+        } finally { // je reviens a  la gestion par defaut : une transaction pour chaque ordre SQL
             con.setAutoCommit(true);
-        }}
-        
-    public static void supprimeSchema(Connection con)
+        }
+    }
+
+    public static void supprimeUtilisateur(Connection con)
             throws SQLException {
-        // je veux que le schema soit entierement cree ou pas du tout
-        // je vais donc gerer explicitement une transaction
         con.setAutoCommit(false);
-        try ( Statement st = con.createStatement()) {
-            // suppression des tables
+        try ( Statement st = con.createStatement()) { // elimination des tables
             st.executeUpdate(
                     """
                     drop table Clients 
                     """);
-            
+
             con.commit();
-            // je retourne dans le mode par defaut de gestion des transaction :
-            // chaque ordre au SGBD sera considere comme une transaction independante
             con.setAutoCommit(true);
         } catch (SQLException ex) {
-            // quelque chose s'est mal passe
-            // j'annule la transaction
             con.rollback();
-            // puis je renvoie l'exeption pour qu'elle puisse eventuellement etre geree (message a  l'utilisateur...)
             throw ex;
         } finally {
-            // je reviens a  la gestion par defaut : une transaction pour chaque ordre SQL
             con.setAutoCommit(true);
-        }      
+        }
     }
-    
-   /* public static donnesUtilisateur{
+
+    public static void createUser(Connection con) throws SQLException {
+        try ( PreparedStatement pst = con.prepareStatement(
+                """
+                insert into Clients (nom,prenom, email, codepostal,pass)
+                values (?,?,?,?,?)
+                    """)) {
+
+            System.out.println("Nom");
+            String nom = Lire.S();
+            System.out.println("Prenom");
+            String prenom = Lire.S();
+            System.out.println("Email");
+            String email = Lire.S();
+            System.out.println("Code postal");
+            String codepostal = Lire.S();
+            System.out.println("Pass");
+            String pass = Lire.S();
+
+            pst.setString(1, nom);
+            pst.setString(2, prenom);
+            pst.setString(3, email);
+            pst.setString(4, codepostal);
+            pst.setString(5, pass);
+            pst.executeUpdate();
+            con.setAutoCommit(false);
+            con.commit();
+            con.setAutoCommit(true);
+        }catch (SQLException ex) {
+            con.rollback();
+            throw ex;
+        } finally {
+            con.setAutoCommit(true);
+        }
+    }
+
+    public static void afficheToutesPersonnes(Connection con)
+            throws SQLException {
+        try ( Statement st = con.createStatement()) {
+            ResultSet res = st.executeQuery(
+                    "select * from clients");
+            while (res.next()) {
+// on peut accéder à une colonne par son nom
+                int id = res.getInt("id");
+                String nom = res.getString("nom");
+// on peut aussi y accéder par son numéro
+// !! numéro 1 pour la première
+                java.sql.Date dn = res.getDate(3);
+                System.out.println(id + " : " + nom + " né le " + dn);
+            }
+        }
+    }
+
+    // public static void completeSchema(Connection con, )
+    //        throws SQLException {
+    /* public static donnesUtilisateur{
             System.out.println("Nom");
             
             System.out.println("Preom");
 }*/
-    
     public static void main(String[] args) {
         try {
             Connection con = defautConnect();
             System.out.println("connection OK");
-            supprimeSchema(con);
+            supprimeUtilisateur(con);
             System.out.println("Table eliminee");
-            creeSchema(con);
+            creeUtilisateur(con);
             System.out.println("creation OK");
-            
+            createUser(con);
+            System.out.println("creation2 OK");
+
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(GestionbD.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
