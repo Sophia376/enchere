26f1a54f6e0a5916a39adaa51b3f4d98cecafc79
Sophia <sophiaespinola@hotmail.com>
2023-01-01 10:05:22 +0100

    publier objet

--- src/main/java/fr/insa/espinola/infos3/Interface/vues/VBoxDroite.java
+++ src/main/java/fr/insa/espinola/infos3/Interface/vues/VBoxDroite.java
@@ -5,6 +5,7 @@
 package fr.insa.espinola.infos3.Interface.vues;
 
 import fr.insa.espinola.infos3.Interface.VuePrincipale;
+import fr.insa.espinola.infos3.Interface.vues.PublierObjet;
 import java.util.Optional;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.VBox;
@@ -13,27 +14,37 @@ import javafx.scene.layout.VBox;
  *
  * @author nicol
  */
-public class VBoxDroite extends VBox{
+public class VBoxDroite extends VBox {
+
     private VuePrincipale main;
     private ToggleButton deconnexion;
+    private ToggleButton propose;
 
     public VBoxDroite(VuePrincipale main) {
         this.main = main;
-        
+
         this.deconnexion = new ToggleButton("Se déconnecter");
         this.deconnexion.setOnAction((event) -> {
             this.Deconnexion();
         });
         this.setSpacing(20);
-        this.setPadding(new javafx.geometry.Insets(20,20,20,20));
+        this.setPadding(new javafx.geometry.Insets(20, 20, 20, 20));
         this.getChildren().add(this.deconnexion);
+
+        this.propose = new ToggleButton("Publier un Objet");
+        this.propose.setOnAction((event) -> {
+            this.main.setPagePrincipale(new PublierObjet(this.main));
+        });
+        this.setSpacing(20);
+        this.setPadding(new javafx.geometry.Insets(20, 20, 20, 20));
+        this.getChildren().add(this.propose);
     }
-    
+
     public void Deconnexion() {
         this.main.getUtilisateurs().setUtilisateur(Optional.empty());
         this.main.setPagePrincipale(new AffichageBienvenue(this.main));
         this.main.setGauche(new Vide(main));
         this.main.setDroite(new Vide(main));
     }
-    
+
 }
