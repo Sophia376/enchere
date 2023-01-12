58307d46008d1539f3023632f4e63cbbb8b33e0d
Sophia <sophiaespinola@hotmail.com>
2023-01-12 14:25:14 +0100

    cat

--- src/main/java/fr/insa/espinola/infos3/Interface/vues/Connexion.java
+++ src/main/java/fr/insa/espinola/infos3/Interface/vues/Connexion.java
@@ -11,60 +11,104 @@ import fr.insa.espinola.infos3.tables.Clients;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Optional;
+import javafx.geometry.Insets;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 
 /**
  *
  * @author nicol
  */
-public class Connexion extends GridPane{
+public class Connexion extends GridPane {
+
     private VuePrincipale main;
-    
     private TextField email;
     private PasswordField pass;
     private ToggleButton seconnecter;
     private ToggleButton retour;
-    
-    public Connexion(VuePrincipale main){
+
+    public Connexion(VuePrincipale main) {
+        this.setHeight(1000);
         this.main = main;
         this.email = new TextField();
         this.pass = new PasswordField();
-        this.seconnecter = new ToggleButton("Se connecter");
+        this.seconnecter = new ToggleButton("Valider");
         this.retour = new ToggleButton("Retour");
-        
+        ButtonBar btnbar = new ButtonBar();
+        btnbar.getButtons().addAll(this.retour, this.seconnecter);
+        Label emailL = new Label("Email ");
+        Label passL = new Label("Mot de passe ");
+
         this.retour.setOnAction((event) -> {
             this.main.setPagePrincipale(new AffichageBienvenue(this.main));
         });
-        
-        int lig = 0;
-        this.add(this.retour, 0, lig);
+
+        int lig = 50;
+        this.add(emailL, 70, lig, 1, 1);
+        this.add(this.email, 71, lig, 1, 1);
         lig++;
-        this.add(new Label("email : "), 0, lig);
-        this.add(this.email, 1, lig);
-        lig ++;
-        this.add(new Label("mot de passe : "), 0, lig);
-        this.add(this.pass, 1, lig);
-        lig ++;
-        this.add(this.seconnecter, 0, lig,2,1);
-        lig ++;
+        this.add(passL, 70, lig, 1, 1);
+        this.add(this.pass, 71, lig, 1, 1);
+        lig++;
+        this.add(btnbar, 71, lig, 1, 1);
+        lig++;
+
+        this.setHgap(5);
+        this.setVgap(5);
+        this.setPadding(new Insets(10, 10, 10, 10));
+
+        emailL.setFont(new Font("Roboto", 20));
+        passL.setFont(new Font("Roboto", 19));
+        emailL.setStyle("-fx-text-fill: white");
+        passL.setStyle("-fx-text-fill: white");
+        emailL.setPrefWidth(150);
+        passL.setPrefSize(150, 50);
+
+        this.email.setPrefWidth(200);
+        this.pass.setPrefWidth(200);
+
+        /*GridPane.setHgrow(emailL, Priority.NEVER);
+        GridPane.setHgrow(passL, Priority.NEVER);
+        GridPane.setHgrow(this.email, Priority.ALWAYS);
+        GridPane.setHgrow(this.pass, Priority.ALWAYS);*/
         this.seconnecter.setOnAction((event) -> {
             this.SeConnecter();
         });
-        
-        
+
+        var image = new Image("https://static.vecteezy.com/system/resources/previews/001/181/686/original/triangle-pattern-abstract-background-in-blue-vector.jpg");
+        var bgImage = new BackgroundImage(
+                image,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(1.0, 1.0, true, true, false, false)
+        );
+
+        this.setPrefSize(main.getWidth(), main.getHeight());
+        this.setMinSize(main.getWidth(), main.getHeight());
+
+        this.setBackground(new Background(bgImage));
+
     }
-    
+
     public void SeConnecter() {
         String mail = this.email.getText();
         String pass = this.pass.getText();
         try {
             Connection con = this.main.getUtilisateurs().getConBdD();
             Optional<Clients> utilisateur = GestionbD.login(con, mail, pass);
-            if(utilisateur.isEmpty()) {
+            if (utilisateur.isEmpty()) {
                 JavaFXUtils.showErrorInAlert("Email ou mot de passe invalide");
             } else {
                 this.main.getUtilisateurs().setUtilisateur(utilisateur);
@@ -74,6 +118,7 @@ public class Connexion extends GridPane{
             }
         } catch (SQLException ex) {
             JavaFXUtils.showErrorInAlert("Problème interne : " + ex.getLocalizedMessage());
-        }        
+        }
     }
+
 }
